d567371422642ac0c141c157779499901be48716
Nicolas D <78985734+Deb4cker@users.noreply.github.com>
2023-04-01 01:31:21 -0300

    Exception handling - findById

---
+++ src/main/java/com/myProjects/My_project/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.myProjects.My_project.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
