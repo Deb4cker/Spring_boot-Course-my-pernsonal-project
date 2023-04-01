e0b8e6590a7b5b54be976c6bc83c19d7bb960198
Nicolas D <78985734+Deb4cker@users.noreply.github.com>
2023-03-31 23:37:40 -0300

    Payment, one-to-one association

--- src/main/java/com/myProjects/My_project/config/TestConfig.java
+++ src/main/java/com/myProjects/My_project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.myProjects.My_project.entities.Category;
 import com.myProjects.My_project.entities.Order;
 import com.myProjects.My_project.entities.OrderItem;
+import com.myProjects.My_project.entities.Payment;
 import com.myProjects.My_project.entities.Product;
 import com.myProjects.My_project.entities.User;
 import com.myProjects.My_project.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2023-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
--- src/main/java/com/myProjects/My_project/entities/Order.java
+++ src/main/java/com/myProjects/My_project/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.myProjects.My_project.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,12 +17,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable{
-
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -39,6 +40,9 @@ public class Order implements Serializable{
 	
 	@OneToMany(mappedBy = "id.product")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {}
 
@@ -82,10 +86,19 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
